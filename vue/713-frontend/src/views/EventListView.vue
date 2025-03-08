8cced50ea76414a411e399ece6a28f83418a9632
kittanai <kittanai_yam@cmu.ac.th>
2025-03-08 17:25:41 +0700

    Add Task 08-09: Add Next and Prev Page

--- vue/713-frontend/src/views/EventListView.vue
+++ vue/713-frontend/src/views/EventListView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue';
 import eventService from '@/services/EventService';
-import {ref} from 'vue'
+import {ref, computed, watchEffect} from 'vue'
 import type {Event} from '@/types'
 const events = ref<Event[]>([])
 
@@ -9,15 +9,44 @@ interface EventResponse {
   data: Event[]
 }
 
-eventService.getEvents().then((response:EventResponse) => {
+interface Props {
+  page:number
+}
+
+const props = defineProps<Props>()
+const page = computed(() => props.page)
+
+watchEffect(() => {
+  eventService
+    .getEvents(page.value, 2)
+    .then((response) => {
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
+
+eventService.getEvents(page.value, 2).then((response) => {
     events.value = response.data
-  })
+})
 </script>
 
 <template>
   <h1>Events For Good</h1>
   <div class='events'>
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <div class="pagination">
+      <RouterLink
+      id="page-prev"
+      :to="{name: 'event-list-view', query: {page: page -1}}"
+      rel="prev"
+      v-if="page !=1">Prev Page</RouterLink>
+      <RouterLink
+      id="page-next"
+      :to="{name: 'event-list-view', query: {page: page +1}}"
+      rel="next">Next Page</RouterLink>
+    </div>
   </div>
 </template>
 
@@ -26,5 +55,24 @@ eventService.getEvents().then((response:EventResponse) => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
